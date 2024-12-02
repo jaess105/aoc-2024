009d6ebf6633110d9f969f8caa21f7fb8918bc31
jaess105 <jaess105@uni-duesseldorf.de>
2024-12-02 22:43:28 +0100

    solve day 1

---
+++ aoc_2024/src/day1.rs
@@ -0,0 +1,95 @@
+use std::{collections::HashMap, fs};
+
+use regex::Regex;
+
+pub fn solve_day1() {
+    const DAY1_FILE: &str = "resources/day01";
+    let content = fs::read_to_string(DAY1_FILE).expect("Could not find input file!");
+
+    let res = solve_a(content);
+    println!("Result of day1 part a is {res}");
+
+    let content = fs::read_to_string(DAY1_FILE).expect("Could not find input file!");
+    let res = solve_b(content);
+    println!("Result of day1 part b is {res}");
+}
+
+fn solve_b(input: String) -> i32 {
+    let lr_nums = parse_input(input);
+
+    let right_dict = lr_nums
+        .iter()
+        .map(|lr| lr[1])
+        .fold(HashMap::new(), |mut agg, num| {
+            let val = agg.remove(&num).unwrap_or(0);
+            agg.insert(num, val + 1);
+
+            agg
+        });
+
+    lr_nums
+        .iter()
+        .map(|lr| lr[0] * right_dict.get(&lr[0]).unwrap_or(&0))
+        .fold(0, |agg, num| agg + num)
+}
+
+fn solve_a(input: String) -> i32 {
+    let lr_nums = parse_input(input);
+
+    let capacity = lr_nums.len();
+    let mut l_nums = Vec::with_capacity(capacity);
+    let mut r_nums = Vec::with_capacity(capacity);
+    for lr_num in lr_nums {
+        l_nums.push(lr_num[0]);
+        r_nums.push(lr_num[1]);
+    }
+
+    l_nums.sort();
+    r_nums.sort();
+
+    l_nums
+        .iter()
+        .zip(r_nums.iter())
+        .map(|(l, r)| i32::abs(l - r))
+        .fold(0, |agg, num| agg + num)
+}
+
+fn parse_input(input: String) -> Vec<Vec<i32>> {
+    let re = Regex::new(r" +").unwrap();
+
+    let lr_nums: Vec<Vec<i32>> = input
+        .split("\n")
+        .map(|line| {
+            re.split(line)
+                .map(|num| i32::from_str_radix(num, 10).unwrap())
+                .collect()
+        })
+        .collect();
+    lr_nums
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day1::solve_b;
+
+    use super::solve_a;
+
+    const TEST_INPUT: &str = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+
+    #[test]
+    fn test_b() {
+        let result = solve_b(TEST_INPUT.into());
+        assert_eq!(result, 31);
+    }
+
+    #[test]
+    fn test_a() {
+        let result = solve_a(TEST_INPUT.into());
+        assert_eq!(result, 11);
+    }
+}
