506b54ae7981e38e2db9888508af28c632b305c4
jaess105 <jaess105@uni-duesseldorf.de>
2024-12-05 20:10:17 +0100

    isntantiate day 5 solution

--- aoc_2024/src/main.rs
+++ aoc_2024/src/main.rs
@@ -5,9 +5,17 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
+mod util;
 
 fn main() {
-    let aocs: Vec<AocDayData> = vec![day1::day1(), day2::day2(), day3::day3(), day4::day4()];
+    let aocs: Vec<AocDayData> = vec![
+        day1::day1(),
+        day2::day2(),
+        day3::day3(),
+        day4::day4(),
+        day5::day(),
+    ];
 
     for aoc in aocs {
         aoc.solve();
